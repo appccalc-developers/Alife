7a536de00d6d476446c791a742f0ccd932d71bdc
Chenghao-Wu <wu_chenghao_happy@126.com>
2026-06-22 22:35:52 +1200

    Fix member admin seed default migration

---
+++ backend/src/Alife.Infrastructure/Persistence/Migrations/20260622112500_SetMemberIsAdminDefault.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alife.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetMemberIsAdminDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_admin",
+                table: "members",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "is_admin",
+                table: "members",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
