2ab83aa15a4f97a197960ba7d8709e71c36d7cd1
Chenghao-Wu <wu_chenghao_happy@126.com>
2026-06-22 14:26:23 +1200

    Add platform role data model

---
+++ backend/src/Alife.Domain/Entities/PlatformRole.cs
@@ -0,0 +1,11 @@
+namespace Alife.Domain.Entities;
+
+public class PlatformRole
+{
+	public int Id { get; set; }
+	public string Code { get; set; } = string.Empty;
+	public string NameJson { get; set; } = "{}";
+	public int Level { get; set; }
+
+	public ICollection<MemberPlatformRole> MemberRoles { get; set; } = [];
+}
